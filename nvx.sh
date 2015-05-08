e7f5f29dac31797be5f21dfe42b0d6a46797da51
Robin Pedersen <robinpeder@gmail.com>
2015-05-08 21:09:35 +0200

    Remove usage comment

--- nvx.sh
+++ nvx.sh
@@ -1,13 +1,5 @@
 #!/bin/sh -e
 
-# Usage:
-#
-#   nvx.sh v0.10.38 npm install babel
-#   nvx.sh v0.10.38 babel script.js
-#
-# Execute a shell command with a given version of Node.js
-# Automatically downloads Node.js on first use.
-
 VERSION="$1"
 MIRROR="http://nodejs.org/dist"
 
