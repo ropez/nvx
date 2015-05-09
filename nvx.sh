d24aedbca95f482835fb25581219ace18a0285c5
Robin Pedersen <robinp@snap.tv>
2015-05-09 10:14:11 +0200

    Better error handling, and general cleanup
    Directory structure is now compatible with nvm, so it's possible to
    share installations by exporting `NVX_DIR=$NVM_DIR`.

--- nvx.sh
+++ nvx.sh
@@ -1,22 +1,60 @@
 #!/bin/sh -e
 
+SCRIPT="$(basename $0)"
+
+die() {
+  echo "[$SCRIPT] $@" >&2
+  exit 1
+}
+
+if [ $# -lt 2 ]; then
+  die "Usage: $SCRIPT NODE_VERSION command..."
+fi
+
 VERSION="$1"
 MIRROR="http://nodejs.org/dist"
 
-if [ -z "$NVXDIR" ]; then
-  NVXDIR="${HOME:-/tmp}/.nvx"
+if [ -z "$NVX_DIR" ]; then
+  NVX_DIR="${HOME:-/tmp}/.nvx"
 fi
 
-# TODO detect OS
-OS="linux-x64"
+get_os() {
+  case `uname -s` in
+    Linux) echo 'linux' ;;
+    *) die "OS not supported" ;;
+  esac
+}
 
-DIRNAME="node-${VERSION}-${OS}"
-TARBALL="$MIRROR/$VERSION/${DIRNAME}.tar.gz"
+get_arch() {
+  case `uname -m` in
+    x86_64)
+      echo 'x64' ;;
+    i*86)
+      echo 'x86' ;;
+    *)
+      die "ARCH not supported" ;;
+  esac
+}
 
-if [ ! -e "$NVXDIR/$DIRNAME" ]; then
-  mkdir -p "$NVXDIR"
-  curl -s "$TARBALL" | tar xz -C "$NVXDIR"
+if [ ! -e "$NVX_DIR/$VERSION" ]; then
+  OS=`get_os`
+  ARCH=`get_arch`
+  DIRNAME="node-${VERSION}-${OS}-${ARCH}"
+  TARBALL="${DIRNAME}.tar.gz"
+  TARBALL_URL="$MIRROR/$VERSION/$TARBALL"
+
+  TMP=$(mktemp --tmpdir -d nvx-XXXXXX)
+  echo "[$SCRIPT] downloading $TARBALL..."
+  curl -sf "$TARBALL_URL" -o "$TMP/$TARBALL" || die "$TARBALL not found"
+  tar xzf "$TMP/$TARBALL" -C "$TMP"
+
+  mkdir -p "$NVX_DIR"
+  mv "$TMP/$DIRNAME" "$NVX_DIR/$VERSION"
+  rm -r "$TMP"
 fi
 
 shift
-exec env PATH="$NVXDIR/$DIRNAME/bin:$PATH" NODE_PATH="$NVXDIR/$DIRNAME/lib/node_modules:$NODE_PATH" "$@"
+exec env \
+  PATH="$NVX_DIR/$VERSION/bin:$PATH" \
+  NODE_PATH="$NVX_DIR/$VERSION/lib/node_modules:$NODE_PATH" \
+  "$@"
